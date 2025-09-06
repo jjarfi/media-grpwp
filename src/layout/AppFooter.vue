7914276d2a7ac72a2254820924c73f0c33cd0a81
GRPWP <wpcnational@gmail.com>
2025-09-06 15:13:13 +0900

    migrasi di sass style.sass

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -1,5 +1,5 @@
 <script setup></script>
 
 <template>
-    <div class="layout-footer">West Papua Data HUB by<a href="#" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">GRPWP</a></div>
+    <div class="layout-footer">West Papua Repository by<a href="#" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">GRPWP</a></div>
 </template>
